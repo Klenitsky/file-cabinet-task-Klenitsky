0cedfa78d2b76f3ba866d9761fffa561366c9980
Klenitsky <kklenitsky@gmail.com>
2021-12-10 22:49:00 +0300

    Add guard clauses to CreateRecord method

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -10,6 +10,46 @@ namespace FileCabinetApp
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short height, decimal weight, char drivingLicenseCategory)
         {
+            if (string.IsNullOrEmpty(firstName))
+            {
+                throw new ArgumentNullException(nameof(firstName));
+            }
+
+            if ((firstName.Length < 2) || (firstName.Length > 60) || string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentException("Invalid First Name", nameof(firstName));
+            }
+
+            if (string.IsNullOrEmpty(lastName))
+            {
+                throw new ArgumentNullException(nameof(lastName));
+            }
+
+            if ((firstName.Length < 2) || (firstName.Length > 60) || string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentException("Invalid Last Name", nameof(firstName));
+            }
+
+            if (DateTime.Compare(dateOfBirth, DateTime.Today) > 0 || (DateTime.Compare(dateOfBirth, new DateTime(1950, 1, 1)) < 0))
+            {
+                throw new ArgumentException("Invalid date", nameof(dateOfBirth));
+            }
+
+            if ((height < 0) || (height > 250))
+            {
+                throw new ArgumentException("Invalid height", nameof(height));
+            }
+
+            if ((weight < 0) || (weight > 180))
+            {
+                throw new ArgumentException("Invalid height", nameof(height));
+            }
+
+            if ((drivingLicenseCategory != 'A') && (drivingLicenseCategory != 'B') && (drivingLicenseCategory != 'C') && (drivingLicenseCategory != 'D'))
+            {
+                throw new ArgumentException("Invalid license", nameof(drivingLicenseCategory));
+            }
+
             var record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
