ddeb24145b3fd57908300a02a2ba0d01035ade9a
Klenitsky <kklenitsky@gmail.com>
2021-12-18 20:36:36 +0300

    Move validation from template method to validation strategy.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -29,7 +29,7 @@ namespace FileCabinetApp
                 throw new ArgumentNullException(nameof(arguments));
             }
 
-            this.ValidateParameters(arguments);
+            this.CreateValidator().ValidateParameters(arguments);
             var record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
@@ -107,45 +107,7 @@ namespace FileCabinetApp
                 if (record.Id == id)
                 {
                     isValid = true;
-                    if (string.IsNullOrEmpty(arguments.FirstName))
-                    {
-                        throw new ArgumentNullException(nameof(arguments));
-                    }
-
-                    if ((arguments.FirstName.Length < 2) || (arguments.FirstName.Length > 60) || string.IsNullOrWhiteSpace(arguments.FirstName))
-                    {
-                        throw new ArgumentException("Invalid First Name", nameof(arguments));
-                    }
-
-                    if (string.IsNullOrEmpty(arguments.LastName))
-                    {
-                        throw new ArgumentNullException(nameof(arguments));
-                    }
-
-                    if ((arguments.LastName.Length < 2) || (arguments.LastName.Length > 60) || string.IsNullOrWhiteSpace(arguments.LastName))
-                    {
-                        throw new ArgumentException("Invalid Last Name", nameof(arguments));
-                    }
-
-                    if (DateTime.Compare(arguments.DateOfBirth, DateTime.Today) > 0 || (DateTime.Compare(arguments.DateOfBirth, new DateTime(1950, 1, 1)) < 0))
-                    {
-                        throw new ArgumentException("Invalid date", nameof(arguments));
-                    }
-
-                    if ((arguments.Height < 0) || (arguments.Height > 250))
-                    {
-                        throw new ArgumentException("Invalid height", nameof(arguments));
-                    }
-
-                    if ((arguments.Weight < 0) || (arguments.Weight > 180))
-                    {
-                        throw new ArgumentException("Invalid height", nameof(arguments));
-                    }
-
-                    if ((arguments.DrivingLicenseCategory != 'A') && (arguments.DrivingLicenseCategory != 'B') && (arguments.DrivingLicenseCategory != 'C') && (arguments.DrivingLicenseCategory != 'D'))
-                    {
-                        throw new ArgumentException("Invalid license", nameof(arguments));
-                    }
+                    this.CreateValidator().ValidateParameters(arguments);
 
                     this.firstNameDictionary[record.FirstName].Remove(record);
                     this.lastNameDictionary[record.LastName].Remove(record);
@@ -229,11 +191,9 @@ namespace FileCabinetApp
         }
 
         /// <summary>
-        /// Validates the parameters.
+        /// Creates parameters validator.
         /// </summary>
-        /// <param name="arguments">Properties of the record.</param>
-        /// <exception cref="ArgumentNullException"> Parameters are null.</exception>
-        /// <exception cref="ArgumentException">One of the parameters is not valid.</exception>
-        protected abstract void ValidateParameters(Arguments arguments);
+        /// <returns>A validator for the parameters.</returns>
+        protected abstract IRecordValidator CreateValidator();
     }
 }
---
+++ FileCabinetApp/IRecordValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// Interface for validators.
+    /// </summary>
+    public interface IRecordValidator
+    {
+        /// <summary>
+        /// Checks the parameters.
+        /// </summary>
+        /// <param name="arguments">Properties of the record.</param>
+        public abstract void ValidateParameters(Arguments arguments);
+    }
+}
